4bf3714857fb339d7f53dd2a033a7b10b1b2d610
Zola <kebba91@outlook.com>
2025-06-17 23:59:55 +0200

    testing terraform-ci

--- Infra/ec2.tf
+++ Infra/ec2.tf
@@ -6,6 +6,7 @@ data "aws_subnet" "subnet1" {
 resource "aws_instance" "example" {
   ami           = "ami-09e6f87a47903347c"
   instance_type = t2.micro
+  key_name      = "Development"
   subnet_id     = data.aws_subnet.subnet1.id
 
   tags = {
