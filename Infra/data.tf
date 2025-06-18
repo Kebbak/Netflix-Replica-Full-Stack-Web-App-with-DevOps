69b9088b6567f43ca94534cc7ca5b1f762c664c6
Zola <kebba91@outlook.com>
2025-06-18 19:32:50 +0200

    provision infrastructure

--- Infra/data.tf
+++ Infra/data.tf
@@ -18,6 +18,6 @@ data "aws_security_group" "default" {
   }
 }
 
-# data "aws_eks_cluster_auth" "cluster" {
-#   name = module.eks_cluster.cluster_name
-# }
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks_cluster.cluster_name
+}
--- Infra/eks.tf
+++ Infra/eks.tf
@@ -1,10 +1,10 @@
-# module "eks_cluster" {
-#   source                 = "./modules/eks"
-#   cluster_name           = var.cluster_name
-#   node_group_name        = var.node_group_name
-#   cluster_version        = var.cluster_version
-#   instance_type          = var.instance_type
-#   subnet_ids             = var.subnet_ids
-#   vpc_security_group_ids = var.vpc_security_group_ids
+module "eks_cluster" {
+  source                 = "./modules/eks"
+  cluster_name           = var.cluster_name
+  node_group_name        = var.node_group_name
+  cluster_version        = var.cluster_version
+  instance_type          = var.instance_type
+  subnet_ids             = var.subnet_ids
+  vpc_security_group_ids = var.vpc_security_group_ids
 
-# }
+}
